cd2cb18be8f2dc3591cf9b3693ff55da2d474e9c
Thomas Seidler <thomas.seidler@ambrosys.de>
2024-05-20 10:48:26 +0200

    Add vision transformer submodule

--- src/critical_images/noise.py
+++ src/critical_images/noise.py
@@ -1,13 +1,12 @@
 import numpy as np
 
+
 class NoiseSchedule:
     def __init__(self, steps: int):
         self.steps = steps
         self.schedule = self._generate_noise_schedule(steps)
         self.cumulative_product_noise = np.cumprod(1.0 - self.schedule, axis=0)
-        self.sqrt_cumulative_product_noise = np.sqrt(
-            self.cumulative_product_noise
-        )
+        self.sqrt_cumulative_product_noise = np.sqrt(self.cumulative_product_noise)
         self.sqrt_one_minus_cumulative_product_noise = np.sqrt(
             1.0 - self.cumulative_product_noise
         )
@@ -15,9 +14,8 @@ class NoiseSchedule:
     def _generate_noise_schedule(self, timesteps: int):
         return linear_noise_schedule(timesteps)
 
-def get_noisy_sample(
-    dataset: np.ndarray, step: int, noise_schedule: NoiseSchedule
-):
+
+def get_noisy_sample(dataset: np.ndarray, step: int, noise_schedule: NoiseSchedule):
     return noise_schedule.sqrt_cumulative_product_noise[
         step
     ] * dataset + noise_schedule.sqrt_one_minus_cumulative_product_noise[
@@ -31,4 +29,3 @@ def linear_noise_schedule(steps):
     beta_start = 0.0001
     beta_end = 0.02
     return np.linspace(beta_start, beta_end, steps)
-
